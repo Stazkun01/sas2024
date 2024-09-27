dc4a396c0fcf23108eaad21f5f850eca966b62a3
Stazkun01 <123759398+Stazkun01@users.noreply.github.com>
2024-09-27 22:24:42 +0100

    3.c

--- mise en situation/3.c
+++ mise en situation/3.c
@@ -4,13 +4,14 @@ int main (){
             printf(" entrer un nombre entier :");
             scanf("%d" ,&n);
         int i;
-         if(n==1)
-        printf("c'est un nombre premier");
-    for (i =2 ;i< n;i++){
+    for (i =2 ;i< n-1;i++){
        if(n % i == 0){
         printf("ce n'est pas un nombre premier\n");
         break;}
     }
- 
+    if(i == n-1){
+ printf("c'est un nombre premier\n");
+ }
+
     return 0;
 }
